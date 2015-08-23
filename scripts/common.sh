b6f385706bb9fa4f950378b396bfdd5ab5098af2
lavvy <lavashonline@gmail.com>
2015-08-23 14:50:05 -0400

    r

--- scripts/common.sh
+++ scripts/common.sh
@@ -141,7 +141,7 @@ function pull_source()
 {
 	ischroot
 	chrootval=$?
-      #	if [ $chrootval == 2 ] || [ $chrootval == 0 ]; then return; fi # Prevent recursive loop
+      	if [ $chrootval == 2 ] || [ $chrootval == 0 ]; then return; fi # Prevent recursive loop
 	if ! command -v unzip >/dev/null 2>&1; then update_sources && verify_action && install_package "unzip" && verify_action; fi
 	if ! command -v git >/dev/null 2>&1; then update_sources && verify_action && install_package "git" && verify_action; fi
 	if ! command -v svn >/dev/null 2>&1; then update_sources && verify_action && install_package "subversion" && verify_action; fi
